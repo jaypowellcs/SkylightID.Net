a0d57e2dc2fef97182c2296706d2e14cdb12f300
Jay Powell <jay.powellcs@gmail.com>
2023-11-19 19:37:12 -0600

    fixed routing

--- client/src/app/Pages/skylight.cal/skylight.cal.component.ts
+++ client/src/app/Pages/skylight.cal/skylight.cal.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-skylight.cal',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './skylight.cal.component.html',
   styleUrl: './skylight.cal.component.scss'
 })
